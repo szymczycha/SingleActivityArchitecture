74b10da83f1b3a5c1d7d1a9af2009380eb518ce7
Szymon Konieczny <95142108+szymczycha@users.noreply.github.com>
2024-06-22 12:19:40 +0200

    got bored, fixed some stuff

--- App03/app/src/main/java/com/example/app03/MainActivity.java
+++ App03/app/src/main/java/com/example/app03/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
                     replaceFragment(new ImageFragment());
                     break;
             }
+
+
             return true;
         });
 
